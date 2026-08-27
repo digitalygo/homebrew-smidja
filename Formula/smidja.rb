8ed4dde48731e11ca735fbe2fcfbeaa10df0c50e
lucanori <github.uncaring594@passmail.net>
2026-08-27 11:59:42 +0200

    feat: bump formula to v0.2.0

--- Formula/smidja.rb
+++ Formula/smidja.rb
@@ -2,7 +2,7 @@ class Smidja < Formula
   desc "Agentic coding harness shipped as a single static binary"
   homepage "https://github.com/digitalygo/smidja"
   url "https://github.com/digitalygo/smidja/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "12faf67e7064c2c150d6ec0e4c13b6a08df48155f8043a4ef253aa6957c887c5"
+  sha256 "154809153725dde8175da640432244c5adf0d7b15348b65030d69c2d210ce774"
   license "MIT"
 
   depends_on "go" => :build
